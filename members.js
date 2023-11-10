bb0031b0c65de8f608b2088f9ee6e0c90c37846c
edwardlopez414 <59494876+edwardlopez414@users.noreply.github.com>
2023-11-10 16:47:48 +0000

    add file skillsmenbers.js commit

---
+++ members.js
@@ -0,0 +1,6 @@
+function skillsmembers() {
+    var skills = document.getElementById("skills");
+    var members = document.getElementById("members");
+    skills.style.display = "none";
+    members.style.display = "block";
+}
